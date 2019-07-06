a9c1dd5023fbc126999977b4576189cb2ac87c7e
EthanShin <avoirom@gmail.com>
2019-07-07 05:22:38 +0900

    create project

---
+++ app/src/main/java/com/example/weatherpollution/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherpollution
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
