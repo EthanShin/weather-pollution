a4370d980a873137d7f030454fb6ca214a6555dc
EthanShin <avoirom@gmail.com>
2019-07-09 03:59:38 +0900

    feat: Display the current weather
    description:
    1. The variable baseTime was assigned without calibrating the current time.
    2. The response message has a different value from the previous one and has been fixed. (fcstValue -> obsrValue, T3H -> T1H, R06 -> RN1)

--- app/src/main/java/com/example/weatherpollution/Service.kt
+++ app/src/main/java/com/example/weatherpollution/Service.kt
@@ -11,8 +11,8 @@ interface Service {
         @Query("ServiceKey") serviceKey: String,
         @Query("base_date") baseDate: String,
         @Query("base_time") baseTime: String,
-        @Query("Nx") nx: Int,
-        @Query("Ny") ny: Int,
+        @Query("nx") nx: Int,
+        @Query("ny") ny: Int,
         @Query("numOfRows") numOfRows: Int,
         @Query("pageNo") pageNo: Int,
         @Query("_type") type: String
