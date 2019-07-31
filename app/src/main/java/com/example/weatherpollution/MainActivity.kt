f15ecd34c25b5df0175e913b794e4b6605e743c6
EthanShin <avoirom@gmail.com>
2019-07-31 12:41:17 +0900

    feat: Request permission
    description:
    1. Add a new build rule under dependencies for the lastest version of
    play-services-location into the build.gradle.
    2. Request ACCESS_FINE_LOCATION permission with the uses-permission
    element in the manifest.
    3. Use checkSelPermission() and requestPermissions() in the
    MainActivity.
    4. Use onRequestPermissionResult() to pass the user response.

--- app/src/main/java/com/example/weatherpollution/MainActivity.kt
+++ app/src/main/java/com/example/weatherpollution/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.weatherpollution
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+private const val MY_PERMISSION_ACCESS_FINE_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        checkPermissions()
+    }
+
+    @SuppressLint("ObsoleteSdkInt")
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSION_ACCESS_FINE_LOCATION)
+        } else {
+            Toast.makeText(this, "Permission has already been granted", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == MY_PERMISSION_ACCESS_FINE_LOCATION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(this, "Permission was granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
